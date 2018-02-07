6ed113836164b9fa704a8fb4e22c54b1a7efc677
Jason Borer <borerjason@gmail.com>
2018-02-07 10:24:05 -0800

    palindrome

--- 30. Permutation palindrome/index.js
+++ 30. Permutation palindrome/index.js
@@ -4,13 +4,49 @@ Write an efficient function that checks whether any permutation of an input stri
 You can assume the input string only contains lowercase letters.
 
 */
+const isAnyPermutationPalindrome = (str) => {
+  const oddItems = new Set();
+  // iterate through str
+  for (let i = 0; i < str.length; i += 1) {
+    // if item is in Set
+    const char = str[i];
+    if (oddItems.has(char)) {
+      oddItems.delete(char);
+    } else {
+      oddItems.add(char);
+    }
 
+  }
+      // remove item from Set
+  
+  return oddItems.size <= 1;
+}
 
+console.log(isAnyPermutationPalindrome('bbcc'));
 
 /*
-const input = "civic";
+const input = "bbc";
+each character appears an even number of times except for one.
 output: boolean whether any permutation is a palindrome 
 
-Strategy: Start generating palindrome
+
+
+Strategy: keep track of how many times each char appears, if more than one character appears an odd number of times then false
+
+Transformation:
+input = bbc
+Set = [];
+1. char = b; Set = ['b'];
+2. char = b; Set = [];
+3. char = c; Set = ['c'];
+
+const isAnyPermutationPalindrome = (str)  => {
+  // iterate through str
+    // if item is in Set
+      // remove item from Set
+    // else, add to Set
+
+  return Set.length === 1;
+}
 
 */
